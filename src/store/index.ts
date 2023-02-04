e55ec05928ccec466528398e41e700a1d4989f16
alchonokk <galunia_suglob@mail.ru>
2023-02-04 17:14:01 +0300

    feat: add themeModeSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import gitHubFetchReducer from './gitHubFetchSlice';
+import themeModeReducer from './themeModeSlice';
 
 const store = configureStore({
   reducer: {
     gitHubFetch: gitHubFetchReducer,
+    themeMode: themeModeReducer,
   },
 });
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
