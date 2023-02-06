581658ddb7dde9016332e274a222440db3f0e20c
Aleksandr <a.teslin90@gmail.com>
2023-02-06 14:46:09 +0300

    feat: move gitHub constants to env

--- src/constants/githubAuthConstants.ts
+++ src/constants/githubAuthConstants.ts
@@ -1,9 +1,7 @@
 enum GITHUB_AUTH {
-  CLIENT_ID = '1a86f6a854cfd5cffcec',
   REDIRECT_URI = 'http://localhost:3000/login',
   URL_PARAM = 'code',
-  // PROXY_URL = 'https://rs-clone-tracker-github-auth.onrender.com',
-  PROXY_URL = 'http://localhost:4000',
+  PROXY_URL = 'https://rs-clone-tracker-github-auth.onrender.com',
 }
 
 export default GITHUB_AUTH;
