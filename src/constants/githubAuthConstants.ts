6401193e54cf89a7eb8ea4eb5e37113c0995babe
Aleksandr <a.teslin90@gmail.com>
2023-02-06 13:04:50 +0300

    feat: add fake branch to test

--- src/constants/githubAuthConstants.ts
+++ src/constants/githubAuthConstants.ts
@@ -2,7 +2,8 @@ enum GITHUB_AUTH {
   CLIENT_ID = '1a86f6a854cfd5cffcec',
   REDIRECT_URI = 'http://localhost:3000/login',
   URL_PARAM = 'code',
-  PROXY_URL = 'https://rs-clone-tracker-github-auth.onrender.com',
+  // PROXY_URL = 'https://rs-clone-tracker-github-auth.onrender.com',
+  PROXY_URL = 'http://localhost:4000',
 }
 
 export default GITHUB_AUTH;
