4c8e632de33bc21c8478846797231287854b931b
Aleksandr <112382387+alextes90@users.noreply.github.com>
2023-02-06 17:04:25 +0300

    Feat: implement GitHub authentication with environment variables
    * feat: add fake branch to test
    
    * feat: move gitHub constants to env
    
    * fix: remove REDIRECT_URI from constants

--- src/constants/githubAuthConstants.ts
+++ src/constants/githubAuthConstants.ts
@@ -1,6 +1,4 @@
 enum GITHUB_AUTH {
-  CLIENT_ID = '1a86f6a854cfd5cffcec',
-  REDIRECT_URI = 'http://localhost:3000/login',
   URL_PARAM = 'code',
   PROXY_URL = 'https://rs-clone-tracker-github-auth.onrender.com',
 }
