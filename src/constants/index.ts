80b238296ac8ee21be84a08b2208166c1e903bf8
alchonokk <galunia_suglob@mail.ru>
2023-02-07 19:18:43 +0300

    feat: add array for mainPage

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,13 @@
 import ROUTES from './appConstants';
 import GithubMembers from './gitHubMembers';
 import GITHUB_AUTH from './githubAuthConstants';
+import MAIN_PAGE_CARDS from './mainPageCards';
 import LOCAL_STORAGE_KEY from './storageConstants';
 
-export { ROUTES, GITHUB_AUTH, LOCAL_STORAGE_KEY, GithubMembers };
+export {
+  ROUTES,
+  GITHUB_AUTH,
+  LOCAL_STORAGE_KEY,
+  GithubMembers,
+  MAIN_PAGE_CARDS,
+};
---
+++ src/constants/mainPageCards.ts
@@ -0,0 +1,20 @@
+const MAIN_PAGE_CARDS = [
+  {
+    title: 'timer',
+    image: './images/timers.jpg',
+  },
+  {
+    title: 'projects',
+    image: './images/project.jpg',
+  },
+  {
+    title: 'clients',
+    image: './images/clients.png',
+  },
+  {
+    title: 'statistics',
+    image: './images/grap.jpg',
+  },
+];
+
+export default MAIN_PAGE_CARDS;
