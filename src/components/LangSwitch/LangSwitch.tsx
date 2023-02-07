99e5c7236ccb8e637377a3cd150202dbd5cafd5e
alchonokk <galunia_suglob@mail.ru>
2023-02-07 17:47:39 +0300

    fix: replace switch to one button

--- src/components/LangSwitch/LangSwitch.tsx
+++ src/components/LangSwitch/LangSwitch.tsx
@@ -1,82 +1,40 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Grid, Switch, Typography } from '@mui/material';
-
-// import { Button } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 
 const LangSwitch = () => {
-  const [checked, setChecked] = useState(false);
-  // const [isEng, setIsEng] = useState(false);
+  const [isEng, setIsEng] = useState(false);
   const { i18n } = useTranslation();
 
-  // function changeLanguage(language: string) {
-  //   i18n.changeLanguage(language);
-  // }
-
-  const changeLanguageSw = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-    if (checked) {
+  const changeLanguage = () => {
+    if (isEng) {
       i18n.changeLanguage('en');
+      setIsEng(false);
     } else {
       i18n.changeLanguage('ru');
+      setIsEng(true);
     }
   };
 
-  // const changeLanguage1 = () => {
-  //   if (isEng) {
-  //     i18n.changeLanguage('en');
-  //     setIsEng(false);
-  //   } else {
-  //     i18n.changeLanguage('ru');
-  //     setIsEng(true);
-  //   }
-  // };
-
   return (
     <Grid container spacing={1}>
-      {/* <Grid item>
-        <Button size="small" variant="contained" onClick={changeLanguage1}>
-          {isEng && (
-            <Typography color="white" variant="body1" component="span">
-              EN
-            </Typography>
-          )}
-          {!isEng && (
-            <Typography color="white" variant="body1" component="span">
-              RU
-            </Typography>
-          )}
-        </Button>
-      </Grid> */}
-      <Grid item container>
-        <Typography color="white" variant="body1" component="span">
-          EN
-        </Typography>
-        <Switch
-          checked={checked}
-          onChange={changeLanguageSw}
-          color="info"
-          size="small"
-        />
-        <Typography color="white">RU</Typography>
-      </Grid>
-      {/* <Grid item>
-        <Button
-          size="small"
-          variant="contained"
-          onClick={() => changeLanguage('en')}>
-          EN
-        </Button>
-      </Grid>
       <Grid item>
         <Button
           size="small"
-          variant="contained"
-          onClick={() => changeLanguage('ru')}>
-          RU
+          onClick={changeLanguage}
+          color="info"
+          sx={{
+            ':hover': {
+              bgcolor: 'info.main',
+              color: 'white',
+            },
+          }}>
+          <Typography color="white" variant="body1" component="span">
+            {isEng ? 'EN' : 'RU'}
+          </Typography>
         </Button>
-      </Grid> */}
+      </Grid>
     </Grid>
   );
 };
