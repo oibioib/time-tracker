2cd2b5c9e41a2b1feec67b29d6a777d00d4aa147
Aleksandr <a.teslin90@gmail.com>
2023-02-10 20:49:22 +0300

    "feat: add sorting of takers"

--- src/constants/apiFly.ts
+++ src/constants/apiFly.ts
@@ -3,4 +3,8 @@ export const BASE_URL = 'https://cloggl.fly.dev';
 export enum FLY_ROUTES {
   TIMERS = 'timers',
   USERS = 'users',
+  USER_TIMERS = 'usertimers',
+  IS_ACTIVE_TIMERS = '?status=active',
 }
+
+export const MILISEC_IN_SEC = 1000;
--- src/pages/DashboardPage/DashboardViews/TrackerView/TrackerView.tsx
+++ src/pages/DashboardPage/DashboardViews/TrackerView/TrackerView.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 
-import { Box, Button, Grid, Paper, TextField } from '@mui/material';
+import { Box, Grid, Paper, TextField } from '@mui/material';
 
 import AddedTask from '../../../../components/AddedTask/AddedTask';
 import EmptyView from '../../../../components/EmptyView/EmptyView';
 import Timer from '../../../../components/Timer/Timer';
-import { LOCAL_TIMER } from '../../../../constants';
 import { BASE_URL, FLY_ROUTES } from '../../../../constants/apiFly';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
-import { setStartTime } from '../../../../store/timeTrackerSlice';
+import { setIsTimerOn, setTimerData } from '../../../../store/timeTrackerSlice';
 
 interface TaskArr {
   taskName: string;
@@ -20,85 +18,82 @@ interface TaskArr {
 
 interface TaskData {
   startTime: number;
-  endTime: number;
+  totalTime: number;
   title: string;
   id: string;
 }
 
-interface FetchingTrackerData {
-  id: TaskData;
-}
-
 const TrackerView = () => {
-  const [taskNamePrinted, setTaskNamePrinted] = useState('');
+  const timerData = useAppSelector((state) => state.timeTracker);
   const [tasksArr, setTasksArr] = useState<TaskArr[]>([]);
-  const [disabledAdd, setDisableAdd] = useState(true);
   const [refreshPage, setRefreshPage] = useState(true);
   const flyUserId = useAppSelector((state) => state.flyUserData.id);
   const dispatch = useAppDispatch();
-  const total = useAppSelector((state) => state.timeTracker.totalTime);
 
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTaskNamePrinted(event.target.value);
-  };
-  const timesStamps = useAppSelector((state) => state.timeTracker);
-
-  const addTaskHandler = async () => {
-    const response = await fetch(`${BASE_URL}/${FLY_ROUTES.TIMERS}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title: taskNamePrinted,
-        startTime: timesStamps.startTime,
-        // endTime: timesStamps.endTime,
-        userId: flyUserId,
-      }),
-      headers: {
-        'Content-type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Could not POST data to DB');
-    }
-    setDisableAdd(true);
-    dispatch(setStartTime({ startTime: 0 }));
-    localStorage.removeItem(LOCAL_TIMER);
-    setTaskNamePrinted('');
-    setRefreshPage(!refreshPage);
+    dispatch(
+      setTimerData({
+        ...timerData,
+        timerTitle: event.target.value,
+      })
+    );
   };
 
   useEffect(() => {
     if (flyUserId) {
       (async () => {
         const response = await fetch(
-          `${BASE_URL}/${FLY_ROUTES.TIMERS}/${flyUserId}`
+          `${BASE_URL}/${FLY_ROUTES.USER_TIMERS}/${flyUserId}`
         );
         if (!response.ok) {
           throw new Error('Could not get new User id');
         }
-        const data: FetchingTrackerData = await response.json();
+        const data: TaskData[] = await response.json();
         console.log(data);
-        const dataArr: TaskArr[] = Object.entries(data).map(
-          (el: [string, TaskData]) => {
+        const dataArr: TaskArr[] = data
+          .sort((a, b) => b.startTime - a.startTime)
+          .map((el: TaskData) => {
             return {
-              id: el[1].id,
-              taskName: el[1].title,
-              taskStart: new Date(+el[1].startTime).toLocaleDateString(
-                'en-US',
-                {
-                  weekday: 'short',
-                  year: 'numeric',
-                  month: 'short',
-                  day: 'numeric',
-                }
-              ),
-              taskTimeSec: el[1].endTime - el[1].startTime,
+              id: el.id,
+              taskName: el.title,
+              taskStart: new Date(+el.startTime).toLocaleDateString('en-US', {
+                weekday: 'short',
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              }),
+              taskTimeSec: +el.totalTime,
             };
-          }
-        );
+          });
         setTasksArr(dataArr);
       })();
     }
   }, [refreshPage, flyUserId]);
+
+  useEffect(() => {
+    if (flyUserId) {
+      (async () => {
+        const response = await fetch(
+          `${BASE_URL}/${FLY_ROUTES.USER_TIMERS}/${flyUserId}${FLY_ROUTES.IS_ACTIVE_TIMERS}`
+        );
+        if (!response.ok) {
+          throw new Error('Could not get information about User active Timers');
+        }
+        const data = await response.json();
+        if (data[0]?.isActive) {
+          dispatch(
+            setTimerData({
+              timerId: data[0].id,
+              timerTitle: data[0].title,
+              totalTime: +data[0].totalTime || 0,
+              startTime: data[0].startTime,
+            })
+          );
+          dispatch(setIsTimerOn(true));
+        }
+      })();
+    }
+  }, [flyUserId, dispatch]);
 
   return (
     <Grid item container pt={2}>
@@ -108,18 +103,16 @@ const TrackerView = () => {
             <Box>
               <TextField
                 placeholder="What are you working on"
-                value={taskNamePrinted}
+                value={timerData.timerTitle}
                 onChange={onChangeHandler}
               />
             </Box>
             <Box sx={{ display: 'flex' }}>
-              <Timer setDisableAdd={setDisableAdd} />
-              <Button
-                onClick={addTaskHandler}
-                variant="contained"
-                disabled={disabledAdd}>
-                Add
-              </Button>
+              <Timer
+                setRefreshPage={setRefreshPage}
+                refreshPage={refreshPage}
+                flyUserId={flyUserId}
+              />
             </Box>
           </Box>
         </Paper>
