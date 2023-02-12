c99105330f377138a59b17e1accb4c4a08a02e98
Aleksandr <a.teslin90@gmail.com>
2023-02-12 14:45:54 +0300

    feat: add server Api

---
+++ src/api/serverApi.ts
@@ -0,0 +1,70 @@
+import { BASE_URL, SERVER_ROUTES } from '../constants/serverConstants';
+
+export const updateTimer = async (
+  title: string,
+  isActive: number,
+  totalTime: number,
+  timerId: string
+) => {
+  const response = await fetch(
+    `${BASE_URL}/${SERVER_ROUTES.TIMERS}/${timerId}`,
+    {
+      method: 'PATCH',
+      body: JSON.stringify({
+        title,
+        isActive,
+        totalTime,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    }
+  );
+  return response;
+};
+
+export const createTimer = async (timerTitle: string, serverUserId: string) => {
+  const response = await fetch(`${BASE_URL}/${SERVER_ROUTES.TIMERS}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title: timerTitle,
+      startTime: Date.now(),
+      userId: serverUserId,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  return response;
+};
+
+export const getUserTimers = async (serverUserId: string) => {
+  const response = await fetch(
+    `${BASE_URL}/${SERVER_ROUTES.USER_TIMERS}/${serverUserId}`
+  );
+  return response;
+};
+
+export const getActiveTimer = async (serverUserId: string) => {
+  const response = await fetch(
+    `${BASE_URL}/${SERVER_ROUTES.USER_TIMERS}/${serverUserId}${SERVER_ROUTES.IS_ACTIVE_TIMERS}`
+  );
+  return response;
+};
+
+export const createServerUserId = async (
+  githubId: number,
+  githubName: string
+) => {
+  const response = await fetch(`${BASE_URL}/${SERVER_ROUTES.USERS}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      githubId,
+      githubName,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response;
+};
