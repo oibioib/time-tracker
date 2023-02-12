3938f606e04f3310bfa483277ec52c0fe8b9995e
Aleksandr <a.teslin90@gmail.com>
2023-02-12 14:47:22 +0300

    feat: add server constants

--- src/constants/serverConstants.ts
+++ src/constants/serverConstants.ts
@@ -1,10 +1,13 @@
 export const BASE_URL = 'https://cloggl.fly.dev';
 
-export enum FLY_ROUTES {
+export enum SERVER_ROUTES {
   TIMERS = 'timers',
   USERS = 'users',
   USER_TIMERS = 'usertimers',
   IS_ACTIVE_TIMERS = '?status=active',
 }
 
-export const MILISEC_IN_SEC = 1000;
+export enum TIMER_ACTIVE {
+  ACTIVE = 1,
+  INACTIVE = 0,
+}
