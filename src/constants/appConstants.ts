585e7d1e9163b1344effed87238aab215cceb551
alchonokk <galunia_suglob@mail.ru>
2023-02-16 13:20:38 +0300

    feat: add constant for duration of day

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -10,3 +10,4 @@ export enum ROUTES {
 export const HOURS_IN_MILISEC = 1000 * 60 * 60;
 export const TASKS_SHOWED_DEFAULT = 5;
 export const MORE_TASKS = 2;
+export const DURATION_OF_DAY = 86399000;
