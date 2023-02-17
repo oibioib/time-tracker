d3a8933c6d9476e9128d426b34f5304b3fd06dfd
alchonokk <galunia_suglob@mail.ru>
2023-02-17 15:43:32 +0300

    refactor: change import

--- src/helpers/convertationToDate.ts
+++
@@ -1,6 +0,0 @@
-const convertationToDate = (time: string) => {
-  const dateFormat = new Date(Number(time));
-  return dateFormat.toLocaleDateString('en-GB');
-};
-
-export default convertationToDate;
--- src/helpers/convertationtoMin.ts
+++
@@ -1,5 +0,0 @@
-const convertationToMin = (timeInMSec: string) => {
-  return Number(timeInMSec) / (1000 * 60);
-};
-
-export default convertationToMin;
