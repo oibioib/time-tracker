60480e52e04e615401be70ed1a7662544555eecc
alchonokk <galunia_suglob@mail.ru>
2023-02-20 00:35:37 +0300

    fix: delete statisticsValueX

--- src/pages/DashboardPage/DashboardViews/StatisticsView/StatisticsView.tsx
+++ src/pages/DashboardPage/DashboardViews/StatisticsView/StatisticsView.tsx
@@ -15,10 +15,7 @@ import { Grid, Typography } from '@mui/material';
 
 import EmptyViewStatistic from '../../../../components/EmptyView/EmptyViewStatistic';
 import ProductivityBox from '../../../../components/Productivity';
-import {
-  CalendarStatistics,
-  SelectX,
-} from '../../../../components/SelectStatistics';
+import CalendarStatistics from '../../../../components/SelectStatistics';
 import {
   DEFAULT_END_TODAY_TIMESTAMP,
   DEFAULT_STARTDAY_PREV_WEEK_TIMESTAMP,
@@ -34,8 +31,6 @@ import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
 import { setErrorMessage } from '../../../../store/errorHandler';
 import {
   addTimePeriod,
-  deleteDataInterval,
-  deleteTotalData,
   getDataInterval,
   getTimersTime,
 } from '../../../../store/statisticSlice';
@@ -69,23 +64,17 @@ const StatisticsView = () => {
     (state) => state.statistics.dataTotalTime
   );
   const intervalData = useAppSelector((state) => state.statistics.dataInterval);
-  const statisticsValueX = useAppSelector((state) => state.statistics.valueX);
   const serverUserId = useAppSelector((state) => state.serverUserData.id);
 
   useEffect(() => {
     if (serverUserId) {
-      if (statisticsValueX === 'tasks') {
-        dispatch(getDataInterval({ serverUserId, rezStartDate, rezEndDate }));
-        dispatch(deleteTotalData());
-      } else {
-        const dayAmount = Math.trunc(
-          (rezEndDate - rezStartDate) / DURATION_OF_DAY
-        );
-        dispatch(getTimersTime({ serverUserId, rezStartDate, dayAmount }));
-        dispatch(deleteDataInterval());
-      }
+      dispatch(getDataInterval({ serverUserId, rezStartDate, rezEndDate }));
+      const dayAmount = Math.trunc(
+        (rezEndDate - rezStartDate) / DURATION_OF_DAY
+      );
+      dispatch(getTimersTime({ serverUserId, rezStartDate, dayAmount }));
     }
-  }, [rezStartDate, rezEndDate, statisticsValueX, dispatch, serverUserId]);
+  }, [rezStartDate, rezEndDate, dispatch, serverUserId]);
 
   if (errorGetDataInterval) {
     dispatch(setErrorMessage(errorGetDataInterval));
@@ -133,26 +122,32 @@ const StatisticsView = () => {
     <Grid item container pt={1}>
       <Typography mb={0}>Statistics Page</Typography>
       <Grid item container justifyContent="space-around">
-        <SelectX />
         <CalendarStatistics />
       </Grid>
 
-      {intervalTotalData.length ? (
-        <Grid item container>
+      {intervalData.length ? (
+        <Grid item container mt={2}>
           <Grid item xs={11} sm={12} maxHeight={400}>
+            <Typography textAlign="center">
+              Time for each task for selected period
+            </Typography>
+            <Bar data={selectedData} options={options} />
+          </Grid>
+        </Grid>
+      ) : null}
+
+      {intervalTotalData.length ? (
+        <Grid item container mt={2}>
+          <Grid item xs={11} sm={12} maxHeight={400}>
+            <Typography textAlign="center">
+              Your WorkTime on day for selected period{' '}
+            </Typography>
             <Bar data={totalTimeData} options={options} />
           </Grid>
           <ProductivityBox />
         </Grid>
       ) : null}
 
-      {intervalData.length ? (
-        <Grid item container>
-          <Grid item xs={11} sm={12} maxHeight={400}>
-            <Bar data={selectedData} options={options} />
-          </Grid>
-        </Grid>
-      ) : null}
       {intervalTotalData.length || intervalData.length ? null : (
         <EmptyViewStatistic />
       )}
