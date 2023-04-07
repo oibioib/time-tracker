aae7fa4c89babd100e5876b201b58fd4fcc8a497
Aleksandr <a.teslin90@gmail.com>
2023-04-07 09:14:27 +0300

    refactor: change base gitHub url

--- src/constants/githubAuthConstants.ts
+++ src/constants/githubAuthConstants.ts
@@ -1,6 +1,6 @@
 enum GITHUB_AUTH {
   URL_PARAM = 'code',
-  PROXY_URL = 'https://rs-clone-tracker-github-auth.onrender.com',
+  PROXY_URL = 'https://cloggl-proxy.fly.dev',
 }
 
 export default GITHUB_AUTH;
